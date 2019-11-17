d207e1b3ebd537aba5e4f99b03b92dbbf03f7278
Welton Vieira dos Santos <welton@gmail.com>
2019-11-17 20:56:37 +0100

    Ejemplo para ordenar tipos primitivos y tipos envoltorios con el método sort de Colletions.

--- JavaAvanzado/src/main/java/com/mitocode/tema6/App.java
+++ JavaAvanzado/src/main/java/com/mitocode/tema6/App.java
@@ -1,4 +1,9 @@
 package com.mitocode.tema6;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Clase de prueba del tema 6.
  * Ordenando Colecciones en Java.
@@ -7,8 +12,25 @@ package com.mitocode.tema6;
 public class App {
 
 	public static void main(String[] args) {
+		List<Integer> listaEntero = new ArrayList<>();
+		listaEntero.add(25);
+		listaEntero.add(10000);
+		listaEntero.add(1);
+		System.out.println("*** Lista de Enteros ***");
+		System.out.println("Lista sin ordenar " + listaEntero);
+		//Para ordenar la lista se hace uso del sort de Colletions
+		Collections.sort(listaEntero);
+		System.out.println("Lista ordenada " + listaEntero);
 		
-
+		List<String> listaString = new ArrayList<>();
+		listaString.add("Welton");
+		listaString.add("Lucia");
+		listaString.add("Joao");		
+		System.out.println("*** Lista de Strings ***");
+		System.out.println("Lista sin ordenar " + listaString);
+		//Para ordenar la lista se hace uso del sort de Colletions
+		Collections.sort(listaString);
+		System.out.println("Lista ordenada " + listaString);
 	}
 
 }
