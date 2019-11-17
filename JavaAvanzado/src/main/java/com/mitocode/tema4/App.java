d11dbea51853198a436d52af954144dc4f5b0a5c
Welton Vieira dos Santos <welton.dossantos@udc.es>
2019-11-17 11:34:32 +0100

    Uso de varios parámetros en la clase generica. Muy importante comprender esa parte del tema.

--- JavaAvanzado/src/main/java/com/mitocode/tema4/App.java
+++ JavaAvanzado/src/main/java/com/mitocode/tema4/App.java
@@ -5,16 +5,14 @@ import java.util.List;
 
 public class App {
 	public static void main(String[] args) {
-		//Concepto de TypeSafety
-		List<String> lista = new ArrayList<>();
-		lista.add("MitoCode");
-		//lista.add(25);//Nos provoca error de compilación debido al uso del tipo entero en lugar de String definido en la lista.
+		Clase<String, Integer, String,Double> c = new Clase<>("MitoCode", 25, "Jaime", 25.5);
 		
-		String[] array = new String[5];
-		array[0] = "MitoCode";
-		//array[1] = 1;//Provoca error de compilación debido al uso del tipo entero en lugar de String, ya que el array almcena String.
+		List<Clase<String,Integer,String,String>> lista = new ArrayList<>();
+		lista.add(new Clase<String,Integer,String,String>("MitoCode",25,"Jaime","sucribete"));//Este caso el diamante puede quedar vacio porque se esta instanciando.
 		
-		
+		for(Clase<String,Integer,String,String> c1 : lista) {
+			c1.mostrarTipo();
+		}
 	}
 
 }
--- JavaAvanzado/src/main/java/com/mitocode/tema4/Clase.java
+++ JavaAvanzado/src/main/java/com/mitocode/tema4/Clase.java
@@ -4,23 +4,27 @@ package com.mitocode.tema4;
  * Para probasr el uso de los genéricos.
  * @author weltonvs
  */
-public class Clase<T> {
-	private T objeto;
-
-	/**
-	 * Método constructor de la clase genérica Clase
-	 * @param objeto T - Tipo genérico.
-	 */
-	public Clase(T objeto) {
-		super();
-		this.objeto = objeto;
-	}
+public class Clase<K,T,V,E> {
+	private K objetoK;
+	private T objetoT;
+	private V objetoV;
+	private E objetoE;
 	
+	public Clase(K objetoK, T objetoT, V objetoV, E objetoE) {		
+		this.objetoK = objetoK;
+		this.objetoT = objetoT;
+		this.objetoV = objetoV;
+		this.objetoE = objetoE;
+	}
+
 	/**
 	 * Método que muestra el tipo de objeto utilizado.
 	 */
 	public void mostrarTipo() {
-		System.out.println("T es un: " + objeto.getClass().getName());
+		System.out.println("K es un: " + objetoK.getClass().getName());
+		System.out.println("T es un: " + objetoT.getClass().getName());
+		System.out.println("V es un: " + objetoV.getClass().getName());
+		System.out.println("E es un: " + objetoE.getClass().getName());
 	}
 	
 
