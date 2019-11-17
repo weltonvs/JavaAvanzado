58e5692007d71863b954fe9d1cabdcfa5cd59964
Welton Vieira dos Santos <welton.dossantos@udc.es>
2019-11-17 12:50:00 +0100

    Creando paquete y clase para el tema 6 - Ordenando Colecciones.

---
+++ JavaAvanzado/src/main/java/com/mitocode/tema6/App.java
@@ -0,0 +1,14 @@
+package com.mitocode.tema6;
+/**
+ * Clase de prueba del tema 6.
+ * Ordenando Colecciones en Java.
+ * @author weltonvs
+ */
+public class App {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
