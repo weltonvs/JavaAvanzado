6f9fac3a85784e1a8c92dc38166521f08f789934
Welton Vieira dos Santos <welton.dossantos@udc.es>
2019-11-17 10:49:21 +0100

    Primera parte de los genéricos. Aqui solo se generaliza el tipo de objeto que va tratar la clase. Hace las pruebas pertinentes con dos ejemplos, uno con una string y otro con un tipo envoltorio Integer.

--- JavaAvanzado/src/main/java/com/mitocode/tema4/App.java
+++ JavaAvanzado/src/main/java/com/mitocode/tema4/App.java
@@ -1,5 +1,14 @@
 package com.mitocode.tema4;
 
 public class App {
+	public static void main(String[] args) {
+		Clase<String> c = new Clase<>("MitoCode");
+		c.mostrarTipo();
+		
+		Clase<Integer> c2 = new Clase<>(2);
+		c2.mostrarTipo();
+		
+		
+	}
 
 }
--- JavaAvanzado/src/main/java/com/mitocode/tema4/Clase.java
+++ JavaAvanzado/src/main/java/com/mitocode/tema4/Clase.java
@@ -1,5 +1,27 @@
 package com.mitocode.tema4;
+/**
+ * Clase Clase
+ * Para probasr el uso de los genéricos.
+ * @author weltonvs
+ */
+public class Clase<T> {
+	private T objeto;
 
-public class Clase {
+	/**
+	 * Método constructor de la clase genérica Clase
+	 * @param objeto T - Tipo genérico.
+	 */
+	public Clase(T objeto) {
+		super();
+		this.objeto = objeto;
+	}
+	
+	/**
+	 * Método que muestra el tipo de objeto utilizado.
+	 */
+	public void mostrarTipo() {
+		System.out.println("T es un: " + objeto.getClass().getName());
+	}
+	
 
 }
