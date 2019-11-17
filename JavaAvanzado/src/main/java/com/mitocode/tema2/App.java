a54b0038dedbd567d3edfe941af316d35e770160
Welton Vieira dos Santos <welton.dossantos@udc.es>
2019-11-17 09:36:38 +0100

    Ejercicio del tema 2 - instanceof terminado

--- JavaAvanzado/src/main/java/com/mitocode/tema2/App.java
+++ JavaAvanzado/src/main/java/com/mitocode/tema2/App.java
@@ -1,4 +1,8 @@
 package com.mitocode.tema2;
+
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Clase App
  * Temario 2 - Hablar de instanceof
@@ -6,6 +10,16 @@ package com.mitocode.tema2;
  *
  */
 public class App {
+	private List canasta = new ArrayList();
+	
+	private void verificar(Object objeto) {
+		if(objeto instanceof Fruta) {
+			canasta.add(objeto);
+			System.out.println("Fruta agregada " + ((Fruta)objeto).getNombre());
+		} else {
+			System.out.println("Elemento no permitido");
+		}
+	}
 
 	public static void main(String[] args) {
 		String texto = new  String("MitoCode");
@@ -22,6 +36,22 @@ public class App {
 		if(a1 instanceof Persona) {
 			System.out.println("a1 tambien es una Persona");
 		}
+		
+		//Se tiene una canasta en donde solo se debe permitir ingreso de frutas.
+		System.out.println("Canasta abierta, por favor ingresar SOLO FRUTAS");
+		
+		
+		Manzana m1 = new Manzana("Roja");
+		Manzana m2 = new Manzana("Verde");
+		Naranja n1 = new Naranja("Naranja sin pepa");
+		Galleta g1 = new Galleta("Chocolate");
+		
+		App app = new App();
+		app.verificar(m1);
+		app.verificar(m2);
+		app.verificar(n1);
+		app.verificar(g1);
+		
 
 	}
 
