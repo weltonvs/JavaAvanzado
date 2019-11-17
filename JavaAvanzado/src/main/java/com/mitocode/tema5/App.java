5b0079eda679f4b914346f6f1ff00da957691d07
Welton Vieira dos Santos <welton.dossantos@udc.es>
2019-11-17 12:46:56 +0100

    Tema 5 - capacidades del arrayList. Solo esta disponible en el arrayList.

---
+++ JavaAvanzado/src/main/java/com/mitocode/tema5/App.java
@@ -0,0 +1,13 @@
+package com.mitocode.tema5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+	public static void main(String[] args) {
+		List<String> lista = new ArrayList<>();
+
+	}
+
+}
