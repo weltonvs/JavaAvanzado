d3e6cb4f501fa90edaa902b1f53dfc6294d04f37
Welton Vieira dos Santos <welton.dossantos@udc.es>
2019-11-17 11:17:55 +0100

    Demostración del uso de TypeSafety en la lista y en el array.

--- JavaAvanzado/src/main/java/com/mitocode/tema4/App.java
+++ JavaAvanzado/src/main/java/com/mitocode/tema4/App.java
@@ -1,12 +1,18 @@
 package com.mitocode.tema4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 	public static void main(String[] args) {
-		Clase<String> c = new Clase<>("MitoCode");
-		c.mostrarTipo();
+		//Concepto de TypeSafety
+		List<String> lista = new ArrayList<>();
+		lista.add("MitoCode");
+		//lista.add(25);//Nos provoca error de compilación debido al uso del tipo entero en lugar de String definido en la lista.
 		
-		Clase<Integer> c2 = new Clase<>(2);
-		c2.mostrarTipo();
+		String[] array = new String[5];
+		array[0] = "MitoCode";
+		//array[1] = 1;//Provoca error de compilación debido al uso del tipo entero en lugar de String, ya que el array almcena String.
 		
 		
 	}
