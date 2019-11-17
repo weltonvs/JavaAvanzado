fda6503c4666cca272bebbe152ec4c77bea6e269
Welton Vieira dos Santos <welton.dossantos@udc.es>
2019-11-17 08:52:37 +0100

    Ejercicio de instancia y stactic del tema 1

---
+++ JavaAvanzado/src/main/java/com/mitocode/tema1/App.java
@@ -0,0 +1,15 @@
+package com.mitocode.tema1;
+
+/**
+ * Clase APP
+ * Utilizado para ejemplo de instancia y static.
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+        System.out.println(Alumno.sumar(1));
+    }
+}
