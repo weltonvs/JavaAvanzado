22b47344a31f83e2e1060287b6c639f875fda4cd
Welton Vieira dos Santos <welton.dossantos@udc.es>
2019-11-18 13:36:42 +0100

    Implementación de la colección TreeSet para elementos simples. Es una estructura que se preocupa por el orden y tambien no permite insertar elementos duplicados.

--- JavaAvanzado/src/main/java/com/mitocode/tema6/treeset/AppTreeSet.java
+++ JavaAvanzado/src/main/java/com/mitocode/tema6/treeset/AppTreeSet.java
@@ -1,4 +1,8 @@
 package com.mitocode.tema6.treeset;
+
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * Clase AppTreeSet
  * Utilizado para hacer pruebas con la colección TreeSet.
@@ -7,7 +11,19 @@ package com.mitocode.tema6.treeset;
 public class AppTreeSet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Colección de elementos simples como un String.
+		Set<String> listaString = new TreeSet<>();
+		listaString.add("MitoCode");
+		listaString.add("Mito");
+		listaString.add("Code");
+		listaString.add("Mito");
+		listaString.add("Jaime");
+		listaString.add("AAA");
+		
+		for (String elemento : listaString) {
+			System.out.println(elemento);
+		}
+		System.out.println("Se observa que se preocupa por el orden de los elementos y tampoco permite duplicados");
 
 	}
 
