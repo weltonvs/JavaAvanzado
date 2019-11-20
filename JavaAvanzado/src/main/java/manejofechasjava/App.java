6996bffa8721a9dbc80208a5f6a42f4899242db0
Welton Vieira dos Santos <welton.dossantos@udc.es>
2019-11-20 11:57:54 +0100

    Utilizando el GregorianCalendar como calendario.

--- JavaAvanzado/src/main/java/manejofechasjava/App.java
+++ JavaAvanzado/src/main/java/manejofechasjava/App.java
@@ -1,6 +1,7 @@
 package manejofechasjava;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * Clase App
@@ -10,14 +11,10 @@ import java.util.Date;
 public class App {
 
 	public static void main(String[] args) {
-		Date fecha = new Date();
-		int anio = fecha.getYear() + 1900;//Hay que sumar 1900 debido a implementación de la api date de java.
-		int dia = fecha.getDate();//Devuelve el dia.
-		System.out.println("Año:" + anio);
-		System.out.println("Dia:" + dia);
-		//Se puede observar que los métodos están desactualizados.
-		//Eso es porque futuramente se va dejar de utilizar por parte de la 
-		//API de java.
+		Calendar calendario = new GregorianCalendar(2015,Calendar.DECEMBER,25);
+		int anio = calendario.get(calendario.YEAR);
+		
+		System.out.println(anio);
 	}
 
 }
