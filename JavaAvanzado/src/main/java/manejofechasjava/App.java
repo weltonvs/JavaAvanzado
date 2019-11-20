770a4ee2808b3cb8fb39fa1d5e207f182c3c8dba
Welton Vieira dos Santos <welton.dossantos@udc.es>
2019-11-20 14:11:05 +0100

    Implementa medir tiempo tiempo y verificar fechas con las versiones 7 y 8 de java para ver las diferencias.

--- JavaAvanzado/src/main/java/manejofechasjava/App.java
+++ JavaAvanzado/src/main/java/manejofechasjava/App.java
@@ -1,8 +1,11 @@
 package manejofechasjava;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
-
 /**
  * Clase App
  * Manejo de fechas en java.
@@ -10,11 +13,54 @@ import java.util.GregorianCalendar;
  */
 public class App {
 
+	public void verificar(int version) {
+		if(version == 7) {
+			Calendar fecha1 = Calendar.getInstance();
+			Calendar fecha2 = Calendar.getInstance();
+			fecha1.set(1991, 0, 21);
+			System.out.println(fecha1.after(fecha2));
+		} else if(version == 8) {
+			LocalDate fecha1 = LocalDate.of(1991, 01, 21);
+			LocalDate fecha2 = LocalDate.now();
+			System.out.println(fecha1.isAfter(fecha2));
+			System.out.println(fecha1.isBefore(fecha2));
+			
+			LocalTime tiempo1 = LocalTime.of(22, 30, 50);
+			LocalTime tiempo2 = LocalTime.now();
+			System.out.println(tiempo1.isAfter(tiempo2));
+			System.out.println(tiempo1.isBefore(tiempo2));
+			
+			LocalDateTime fechaTiempo1 = LocalDateTime.of(1991,1,21,22, 30, 50);
+			LocalDateTime fechaTiempo2 = LocalDateTime.now();
+			System.out.println(fechaTiempo1.isAfter(fechaTiempo2));
+			System.out.println(fechaTiempo1.isBefore(fechaTiempo2));
+		}
+	}
+	
+	public void medirTiempo(int version) throws InterruptedException{
+		if(version == 7) {
+			long ini = System.currentTimeMillis();
+			Thread.sleep(1000);
+			long fin = System.currentTimeMillis();
+			System.out.println(fin - ini);
+		} else if (version == 8) {
+			Instant ini = Instant.now();
+			Thread.sleep(1000);
+			Instant fin = Instant.now();
+			System.out.println(Duration.between(ini, fin).toMillis());
+		}
+	}
+	
 	public static void main(String[] args) {
-		Calendar calendario = new GregorianCalendar(2015,Calendar.DECEMBER,25);
-		int anio = calendario.get(calendario.YEAR);
+		App app = new App();
+		try {
+			app.verificar(8);
+			app.medirTiempo(7);
+			app.medirTiempo(8);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-		System.out.println(anio);
 	}
 
 }
